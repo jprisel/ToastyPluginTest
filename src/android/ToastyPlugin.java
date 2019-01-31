c400aced3995f3bd20de3333fe764db8a656d27e
João Rodrigues <a35392@alunos.isel.ipl.pt>
2019-01-31 17:25:19 +0000

    Update ToastyPlugin.java

--- src/android/ToastyPlugin.java
+++ src/android/ToastyPlugin.java
@@ -1,157 +1,158 @@
-            package com.stanleyidesis.cordova.plugin;
-            // The native Toast API
-            import android.widget.Toast;
-            import android.content.Context;
-            // Cordova-required packages
-            import org.apache.cordova.CallbackContext;
-            import org.apache.cordova.CordovaPlugin;
-            import org.apache.cordova.PluginResult;
-            import org.json.JSONArray;
-            import org.json.JSONException;
-            import org.json.JSONObject;
+                package com.stanleyidesis.cordova.plugin;
+                // The native Toast API
+                import android.widget.Toast;
+                import android.content.Context;
+                // Cordova-required packages
+                import org.apache.cordova.CallbackContext;
+                import org.apache.cordova.CordovaPlugin;
+                import org.apache.cordova.PluginResult;
+                import org.json.JSONArray;
+                import org.json.JSONException;
+                import org.json.JSONObject;
 
-            //The LOQR API
-            import io.loqr.loqrCMSDK.Loqr;
-            import io.loqr.loqrCMSDK.LoqrException;
-            import io.loqr.loqrCMSDK.*;
-            import io.loqr.loqrOMSDK.*;
-            import io.loqr.loqrOMSDK.utils.*;
-
-
-            //Permissions
-            import android.support.v4.app.ActivityCompat;
-            import android.Manifest;
+                //The LOQR API
+                import io.loqr.loqrCMSDK.Loqr;
+                import io.loqr.loqrCMSDK.LoqrException;
+                import io.loqr.loqrCMSDK.*;
+                import io.loqr.loqrOMSDK.*;
+                import io.loqr.loqrOMSDK.utils.*;
 
 
-
-            public class ToastyPlugin extends CordovaPlugin {
-              private static final String DURATION_LONG = "long";
-              @Override
-              public boolean execute(String action, JSONArray args,
-                final CallbackContext callbackContext) {
+                //Permissions
+                import android.support.v4.app.ActivityCompat;
+                import android.Manifest;
 
 
-                  String message;
-                  String duration;
-                  try {
-                    JSONObject options = args.getJSONObject(0);
-                    message = options.getString("message");
-                    duration = options.getString("duration");
-                  } catch (JSONException e) {
-                    callbackContext.error("Error encountered: " + e.getMessage());
-                    return false;
-                  }
 
-                  if (!Loqr.permissionsChecked(cordova.getActivity())) {
-                        // Create the toast
-                    Toast toast3 = Toast.makeText(cordova.getActivity(), "nao tenho permissões, vou pedir",
-                    Toast.LENGTH_LONG);
-                   // Display toast
-                    toast3.show();
-                      ActivityCompat.requestPermissions(cordova.getActivity(),
-                            new String[]{Manifest.permission.READ_PHONE_STATE,
-                                    Manifest.permission.ACCESS_FINE_LOCATION},
-                            27 //use a unique code to distinguish the request results
-                    );
+                public class ToastyPlugin extends CordovaPlugin {
+                  private static final String DURATION_LONG = "long";
+                  @Override
+                  public boolean execute(String action, JSONArray args,
+                    final CallbackContext callbackContext) {
+
+
+                      String message;
+                      String duration;
+                      try {
+                        JSONObject options = args.getJSONObject(0);
+                        message = options.getString("message");
+                        duration = options.getString("duration");
+                      } catch (JSONException e) {
+                        callbackContext.error("Error encountered: " + e.getMessage());
+                        return false;
+                      }
+
+                      if (!Loqr.permissionsChecked(cordova.getActivity())) {
+                            // Create the toast
+                        Toast toast3 = Toast.makeText(cordova.getActivity(), "nao tenho permissões, vou pedir",
+                        Toast.LENGTH_LONG);
+                       // Display toast
+                        toast3.show();
+                          ActivityCompat.requestPermissions(cordova.getActivity(),
+                                new String[]{Manifest.permission.READ_PHONE_STATE,
+                                        Manifest.permission.ACCESS_FINE_LOCATION},
+                                27 //use a unique code to distinguish the request results
+                        );
+
+                    }
                       else {
                           switch (action) {
-                      case "show":
-                          show(cordova.getActivity(), message);
-                          break;
-                      case "initLoqr":
-                          initLoqr(cordova.getActivity());
-                          break;
-                      default:
-                          callbackContext.error("\"" + action + "\" is not a recognized action.");
-                          return false;
-                  }
+                          case "show":
+                              show(cordova.getActivity(), message);
+                              break;
+                          case "initLoqr":
+                              initLoqr(cordova.getActivity());
+                              break;
+                          default:
+                              callbackContext.error("\"" + action + "\" is not a recognized action.");
+                              return false;
                       }
-                }
+                          }
 
 
 
-                  PluginResult pluginResult = new PluginResult(PluginResult.Status.OK);
-                  callbackContext.sendPluginResult(pluginResult);
-                  return true;
-              }
-                /*
-                private static void CreateOnboarding (Context context, String email, String name) 
-                {
-                    // Create the toast
-                    Toast toast = Toast.makeText(context, "Vou criar o onboarding", Toast.LENGTH_LONG);
-                   // Display toast
-                    toast.show();
+                      PluginResult pluginResult = new PluginResult(PluginResult.Status.OK);
+                      callbackContext.sendPluginResult(pluginResult);
+                      return true;
+                  }
+                    /*
+                    private static void CreateOnboarding (Context context, String email, String name) 
+                    {
+                        // Create the toast
+                        Toast toast = Toast.makeText(context, "Vou criar o onboarding", Toast.LENGTH_LONG);
+                       // Display toast
+                        toast.show();
 
-                    JSONObject customData = null;
-                    try {
-                        customData = new JSONObject().put("email", email).put("shortName", name).put("acceptPrivacyPolicy", true);
-                    }  catch (Exception e) {
-                    // Create the toast
-                    Toast toast5 = Toast.makeText(context, "Vou criar o onboarding",
-                    Toast.LENGTH_LONG);
-                   // Display toast
-                    toast5.show();
-
-
-                  } 
-
-                    OnLoqrOnboardingResult onboardingResult = new OnLoqrOnboardingResult()  {
-
-                    @Override
-                    public void onError(LoqrException e) {
-                        String aux = "Fail";
-                    }
-                    @Override
-                    public void onCompleted() {
-                        String aux = "OLEEE";
-                    }
+                        JSONObject customData = null;
+                        try {
+                            customData = new JSONObject().put("email", email).put("shortName", name).put("acceptPrivacyPolicy", true);
+                        }  catch (Exception e) {
+                        // Create the toast
+                        Toast toast5 = Toast.makeText(context, "Vou criar o onboarding",
+                        Toast.LENGTH_LONG);
+                       // Display toast
+                        toast5.show();
 
 
-                    };
+                      } 
 
-                    LoqrOnBoarding onBoarding = new LoqrOnBoarding(context);
-                    onBoarding.createProcess("+351", "912992454", customData, onboardingResult);
-                }
-                        
-                */
-                private static void show(Context context, String message, String duration) {
-                    // Create the toast
-                    Toast toast = Toast.makeText(context, message, Toast.LENGTH_LONG);
-                   // Display toast
-                    toast.show();
-                }
+                        OnLoqrOnboardingResult onboardingResult = new OnLoqrOnboardingResult()  {
 
-                private static void initLoqr(Context context)
-                {
-                    // Create the toast
-                    Toast toast = Toast.makeText(cordova.getActivity(), "Vou tentar chamar o Loqr", Toast.LENGTH_LONG);
-                   // Display toast
-                    toast.show();
-
-                  try {
-                      new Loqr(context).initLoqr("LOQR_DEMO_ID", "LOQR_DEMO_KEY", "OLA", new OnLoqrRequestListener() {
-
-                          @Override
-                        public void loqrResponse(int requestCode, String message, Boolean status) {
-                        if (status) {
-                             message = "FUNCIONOU!!";
-                             //CreateOnboarding(cordova.getActivity(), "joao.rodrigues@infosistema.com", "Joao Rodrigues");
+                        @Override
+                        public void onError(LoqrException e) {
+                            String aux = "Fail";
+                        }
+                        @Override
+                        public void onCompleted() {
+                            String aux = "OLEEE";
                         }
 
+
+                        };
+
+                        LoqrOnBoarding onBoarding = new LoqrOnBoarding(context);
+                        onBoarding.createProcess("+351", "912992454", customData, onboardingResult);
+                    }
+
+                    */
+                    private static void show(Context context, String message, String duration) {
                         // Create the toast
-                    Toast toast2 = Toast.makeText(context, message, Toast.LENGTH_LONG);
-                    toast2.show();
-                     }
+                        Toast toast = Toast.makeText(context, message, Toast.LENGTH_LONG);
+                       // Display toast
+                        toast.show();
+                    }
 
-                    },
-                    0); //use a unique code to distinguish the request results
-                  }
-                  catch (Exception e) {
-                     // Display toast
-                    toast.show();
+                    private static void initLoqr(Context context)
+                    {
+                        // Create the toast
+                        Toast toast = Toast.makeText(cordova.getActivity(), "Vou tentar chamar o Loqr", Toast.LENGTH_LONG);
+                       // Display toast
+                        toast.show();
+
+                      try {
+                          new Loqr(context).initLoqr("LOQR_DEMO_ID", "LOQR_DEMO_KEY", "OLA", new OnLoqrRequestListener() {
+
+                              @Override
+                            public void loqrResponse(int requestCode, String message, Boolean status) {
+                            if (status) {
+                                 message = "FUNCIONOU!!";
+                                 //CreateOnboarding(cordova.getActivity(), "joao.rodrigues@infosistema.com", "Joao Rodrigues");
+                            }
+
+                            // Create the toast
+                        Toast toast2 = Toast.makeText(context, message, Toast.LENGTH_LONG);
+                        toast2.show();
+                         }
+
+                        },
+                        0); //use a unique code to distinguish the request results
+                      }
+                      catch (Exception e) {
+                         // Display toast
+                        toast.show();
 
 
-                  }
+                      }
 
+                    }
                 }
-            }
