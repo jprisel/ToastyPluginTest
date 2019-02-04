77b4f44108e730a96f99fe866f6078e442acf5a2
João Rodrigues <a35392@alunos.isel.ipl.pt>
2019-02-04 11:41:18 +0000

    Update NewActivity.java

--- src/android/NewActivity.java
+++ src/android/NewActivity.java
@@ -3,6 +3,9 @@ package com.stanleyidesis.cordova.plugin;
 import android.app.Activity;
 import android.os.Bundle;
 
+// The native Toast API
+                import android.widget.Toast;
+
  //The LOQR API
                 import io.loqr.loqrCMSDK.Loqr;
                 import io.loqr.loqrCMSDK.OnLoqrRequestListener;
@@ -12,18 +15,16 @@ import android.os.Bundle;
 
 public class NewActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        String package_name = getApplication().getPackageName();
-        setContentView(getApplication().getResources().getIdentifier("activity_new", "layout", package_name));
-    }
-    
+   
     
     
     @Override
     public void onNewIntent(Intent intent) {
         super.onNewIntent(intent);
+         // Create the toast
+                        Toast toast = Toast.makeText(context, "Estou no Intend", Toast.LENGTH_LONG);
+                       // Display toast
+                        toast.show();
         if (intent.getAction() != null && intent.getAction().equals("android.intent.action.VIEW")) {
             //Example on how to validate the mobile for onboarding cases
             new LoqrOnBoarding(this).validatePhone("http://loqr.io/demo", intent.getDataString(), new OnPhoneValidation);
