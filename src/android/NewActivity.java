25c192387f14aa6235b194c767893410446b8377
João Rodrigues <a35392@alunos.isel.ipl.pt>
2019-02-04 14:26:27 +0000

    test

--- src/android/NewActivity.java
+++ src/android/NewActivity.java
@@ -1,42 +1,42 @@
-package com.stanleyidesis.cordova.plugin;
+    package com.stanleyidesis.cordova.plugin;
 
-import android.app.Activity;
-import android.os.Bundle;
+    import android.app.Activity;
+    import android.os.Bundle;
 
-// The native Toast API
-                import android.widget.Toast;
+    // The native Toast API
+    import android.widget.Toast;
 
- //The LOQR API
-                import io.loqr.loqrCMSDK.Loqr;
-                import io.loqr.loqrCMSDK.OnLoqrRequestListener;
-                import io.loqr.loqrCMSDK.error.LoqrException;
-                import io.loqr.loqrOMSDK.LoqrOnBoarding;
-                import io.loqr.loqrOMSDK.utils.OnLoqrOnboardingResult;
+     //The LOQR API
+    import io.loqr.loqrCMSDK.Loqr;
+    import io.loqr.loqrCMSDK.OnLoqrRequestListener;
+    import io.loqr.loqrCMSDK.error.LoqrException;
+    import io.loqr.loqrOMSDK.LoqrOnBoarding;
+    import io.loqr.loqrOMSDK.utils.OnLoqrOnboardingResult;
 
-public class NewActivity extends Activity {
+    public class NewActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        String package_name = getApplication().getPackageName();
-        setContentView(getApplication().getResources().getIdentifier("activity_new", "layout", package_name));
-         // Create the toast
-        Toast toast = Toast.makeText(context, "Estou no OnCreate", Toast.LENGTH_LONG);
-        // Display toast
-        toast.show();
-    }
-    
-    
-    
-    @Override
-    public void onNewIntent(Intent intent) {
-        super.onNewIntent(intent);
-         Toast toast = Toast.makeText(context, "Estou no OnNewIntent", Toast.LENGTH_LONG);
-        // Display toast
-        toast.show();
-        if (intent.getAction() != null && intent.getAction().equals("android.intent.action.VIEW")) {
-            //Example on how to validate the mobile for onboarding cases
-            new LoqrOnBoarding(this).validatePhone("http://loqr.io/demo", intent.getDataString(), new OnPhoneValidation);
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            String package_name = getApplication().getPackageName();
+            setContentView(getApplication().getResources().getIdentifier("activity_new", "layout", package_name));
+             // Create the toast
+            Toast toast = Toast.makeText(context, "Estou no OnCreate", Toast.LENGTH_LONG);
+            // Display toast
+            toast.show();
+        }
+
+
+
+        @Override
+        public void onNewIntent(Intent intent) {
+            super.onNewIntent(intent);
+             Toast toast = Toast.makeText(context, "Estou no OnNewIntent", Toast.LENGTH_LONG);
+            // Display toast
+            toast.show();
+            if (intent.getAction() != null && intent.getAction().equals("android.intent.action.VIEW")) {
+                //Example on how to validate the mobile for onboarding cases
+                new LoqrOnBoarding(this).validatePhone("http://loqr.io/demo", intent.getDataString(), new OnPhoneValidation());
+            }
         }
     }
-}
--- src/android/ToastyPlugin.java
+++ src/android/ToastyPlugin.java
@@ -283,30 +283,3 @@
 
 
 
-public class NewActivity extends Activity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        String package_name = getApplication().getPackageName();
-        setContentView(getApplication().getResources().getIdentifier("activity_new", "layout", package_name));
-         // Create the toast
-        Toast toast = Toast.makeText(context, "Estou no OnCreate", Toast.LENGTH_LONG);
-        // Display toast
-        toast.show();
-    }
-    
-    
-    
-    @Override
-    public void onNewIntent(Intent intent) {
-        super.onNewIntent(intent);
-         Toast toast = Toast.makeText(context, "Estou no OnNewIntent", Toast.LENGTH_LONG);
-        // Display toast
-        toast.show();
-        if (intent.getAction() != null && intent.getAction().equals("android.intent.action.VIEW")) {
-            //Example on how to validate the mobile for onboarding cases
-            new LoqrOnBoarding(this).validatePhone("http://loqr.io/demo", intent.getDataString(), new OnPhoneValidation());
-        }
-    }
-}
