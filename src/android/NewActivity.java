490810fb1a04830651f117b3248d12b47efde63f
João Rodrigues <a35392@alunos.isel.ipl.pt>
2019-02-04 11:26:06 +0000

    Update NewActivity.java

--- src/android/NewActivity.java
+++ src/android/NewActivity.java
@@ -3,6 +3,13 @@ package com.stanleyidesis.cordova.plugin;
 import android.app.Activity;
 import android.os.Bundle;
 
+ //The LOQR API
+                import io.loqr.loqrCMSDK.Loqr;
+                import io.loqr.loqrCMSDK.OnLoqrRequestListener;
+                import io.loqr.loqrCMSDK.error.LoqrException;
+                import io.loqr.loqrOMSDK.LoqrOnBoarding;
+                import io.loqr.loqrOMSDK.utils.OnLoqrOnboardingResult;
+
 public class NewActivity extends Activity {
 
     @Override
@@ -10,5 +17,16 @@ public class NewActivity extends Activity {
         super.onCreate(savedInstanceState);
         String package_name = getApplication().getPackageName();
         setContentView(getApplication().getResources().getIdentifier("activity_new", "layout", package_name));
+    }
+    
+    
+    
+    @Override
+    public void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        if (intent.getAction() != null && intent.getAction().equals("android.intent.action.VIEW")) {
+            //Example on how to validate the mobile for onboarding cases
+            new LoqrOnBoarding(this).validatePhone("http://loqr.io/demo", intent.getDataString(), new OnPhoneValidation);
+        }
     }
 }
