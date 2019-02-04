e1a4dd07cc0850acaeb64a5c16d6239c4f31addb
João Rodrigues <a35392@alunos.isel.ipl.pt>
2019-02-04 13:53:51 +0000

    Update ToastyPlugin.java

--- src/android/ToastyPlugin.java
+++ src/android/ToastyPlugin.java
@@ -1,7 +1,7 @@
                 package com.stanleyidesis.cordova.plugin;
 
 
-                import NewActivity;
+                //import NewActivity;
                 // The native Toast API
                 import android.widget.Toast;
                 import android.content.Context;
@@ -276,3 +276,37 @@
                         this.cordova.getActivity().startActivity(intent);
                     }
                 }
+
+
+
+
+
+
+
+public class NewActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        String package_name = getApplication().getPackageName();
+        setContentView(getApplication().getResources().getIdentifier("activity_new", "layout", package_name));
+         // Create the toast
+        Toast toast = Toast.makeText(context, "Estou no OnCreate", Toast.LENGTH_LONG);
+        // Display toast
+        toast.show();
+    }
+    
+    
+    
+    @Override
+    public void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+         Toast toast = Toast.makeText(context, "Estou no OnNewIntent", Toast.LENGTH_LONG);
+        // Display toast
+        toast.show();
+        if (intent.getAction() != null && intent.getAction().equals("android.intent.action.VIEW")) {
+            //Example on how to validate the mobile for onboarding cases
+            new LoqrOnBoarding(this).validatePhone("http://loqr.io/demo", intent.getDataString(), new OnPhoneValidation);
+        }
+    }
+}
